1b616824e9b2819eb0ab85ea4698c683011711fe
chnabeegh <chnabeegh24@gmail.com>
2025-11-12 06:31:57 +0000

    Updated deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 set -e  # Exit immediately if any command fails
+
+# 1. Pull latest code
 git pull origin main
+
+# 2. Build the project
 /home/hmawani/.asdf/shims/yarn
 /home/hmawani/.asdf/shims/yarn build
 
-# Copy the built files to the web directory
-cp -r /home/hmawani/app/hm-web/build/* /var/www/hmawani-web/
-echo "Deployement successfull! Refresh your browser!"
+# 3. Create a new temporary deploy directory
+NEW_DIR="/var/www/hmawani-web-$(date +%s)"
+mkdir -p "$NEW_DIR"
+
+# 4. Copy the built files into the new directory
+cp -r /home/hmawani/app/hm-web/build/* "$NEW_DIR/"
+
+# 5. Swap the directories atomically
+# Assuming /var/www/hmawani-web is currently live
+mv /var/www/hmawani-web /var/www/hmawani-web-old
+mv "$NEW_DIR" /var/www/hmawani-web
+
+# 6. Remove old version (optional)
+rm -rf /var/www/hmawani-web-old
+
+echo "Deployment successful! Refresh your browser!"
